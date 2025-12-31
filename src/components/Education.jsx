b4228770ea0d468a9c00ae3ab0577508d80d6baf
shiv4705 <shivpatel4705.sp@gmail.com>
2025-12-31 15:34:04 +0530

    Transcript added

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Education.scss";
+import transcriptPdf from "../assets/Transcript_22IT110.pdf";
 
 const Education = () => (
   <section id="education">
@@ -7,7 +8,12 @@ const Education = () => (
     <div className="card">
       <h3>B.Tech - Information Technology Engineering</h3>
       <h4>CHARUSAT University (2022 – 2026)</h4>
-      <p>CGPA: 8.49</p>
+      <p>CGPA: 8.48</p>
+      <p>
+        <a href={transcriptPdf} target="_blank" rel="noreferrer">
+          Transcript
+        </a>
+      </p>
     </div>
     <div className="card">
       <h3>XII - SCIENCE (GSEB)</h3>
